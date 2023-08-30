6ced2b8700af7111029485d23a17c04cd8c478d0
RitaLing1021 <106168868+RitaLing1021@users.noreply.github.com>
2023-08-30 18:03:42 +0800

    學習if else switch的判斷式

--- js/all.js
+++ js/all.js
@@ -34,9 +34,56 @@ document.getElementById('calcuLate').onclick = function() {
 
 let member = false;
 let birthday = true;
-let andCheck = member == true && birthday == true;
-let orCheck = member == true || birthday == true;
 
 
-document.getElementById('andCheck').textContent = andCheck;
-document.getElementById('orCheck').textContent = orCheck;
+if (andCheck = member == true && birthday == true){
+  document.getElementById('andCheck').textContent = '是的，我可以使用折扣';
+}else{
+  document.getElementById('andCheck').textContent = '我不符合資格';
+}
+
+if (orCheck = member == true || birthday == true){
+  document.getElementById('orCheck').textContent = '太棒了我可以折扣';
+}else{
+  document.getElementById('orCheck').textContent = '我不符合資格';
+}
+
+
+let guest = 1500;
+
+function sopping(gift){
+  document.getElementById('shoppingGiftA').textContent = '那我可以換' + gift;
+}
+
+function shoppingGift(){
+  if ( guest >= 1000 ){
+    sopping('布偶');
+  }
+  else if( guest >=500 && guest >300 ){
+    sopping('收納箱');
+  }
+  else if( guest >=300 && guest >200 ){
+    sopping('購物袋');
+  }
+  else{
+    sopping('一個寂寞');
+  };  
+}
+
+shoppingGift();
+
+
+let betray = '劉宗翰';
+
+switch(betray){
+  case '林岳廷':
+    alert('賣賣王');
+  break;
+  case '邱品鈞':
+    alert('你敷衍我');
+  break;
+  case '劉宗翰':
+    alert('你敷衍我好像蠻久了');
+  break;
+  default:
+}
